0dcbced7b761d1b20abbb856441d6e68b10cf1d8
Poly Goveia <poly.goveia@gmail.com>
2019-09-14 20:27:56 -0700

    MVP

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const userRouter = require("./Router/userRouter");
+const userRouter = require("./users/userRouter");
 const server = express();
+
 server.use(express.json());
+
 server.use("/api", userRouter);
+
 server.get("/", (req, res) => {
   res.status(200).json("working");
 });
+
 module.exports = server;
--- server.spec.js
+++ server.spec.js
@@ -1,4 +1,79 @@
 const db = require("./data/db-Config");
-const router = require("./Router/userRouter");
+const users = require("./users/userRouter");
 const server = require("./server");
 const request = require("supertest");
+
+describe("GET /", () => {
+  it("get server.js", () => {
+    return request(server)
+      .get("/")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+
+  it("get router.js", () => {
+    return request(server)
+      .get("/api")
+      .then(res => {
+        expect(res.status).toBe(200);
+      });
+  });
+  it("get all users through router", () => {
+    return request(server)
+      .get("/api/users")
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe("application/json");
+      });
+  });
+});
+
+describe.skip("POST requests", () => {
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+  it("succcessful post to users", () => {
+    return request(server)
+      .post("/api/users")
+      .send({ username: "Gabba", password: "Test" })
+      .then(res => {
+        expect(res.status).toBe(200);
+        expect(res.type).toBe("application/json");
+        expect(res.body).toEqual({ username: "Gabba", password: "Test" });
+      });
+  });
+  it("unsuccessful post to users", () => {
+    return request(server)
+      .post("/api/users")
+      .send({ username: "Gabba" })
+      .then(res => {
+        expect(res.status).toBe(500);
+        expect(res.type).toBe("application/json");
+        expect(res.body).toEqual({ error: "bad request" });
+      });
+  });
+});
+
+// Only runs successfully if:
+// 1) Post is skipped - beforeEach.truncate() wipes the database
+// 2) npx knex seed:run - the delete request goes thru, user
+// 5 is deleted after the first test run
+describe("DELETE request", () => {
+  it("successful delete user by ID", () => {
+    return request(server)
+      .delete("/api/users/5")
+      .then(res => {
+        expect(res.status).toBe(201);
+        expect(res.body).toBe(0);
+      });
+  });
+  it("unsuccessful deletes user by ID", () => {
+    return request(server)
+      .delete("/api/users/999999")
+      .then(res => {
+        expect(res.status).toBe(201);
+        expect(res.body).toBe(0);
+      });
+  });
+});
